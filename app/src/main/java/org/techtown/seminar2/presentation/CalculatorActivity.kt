c9feb2fb913ef1acb15d16704191e59b2aa0ca7d
murjune <sis9221302@gmail.com>
2022-06-22 16:43:20 +0900

    [FEAT] 초기세팅

--- app/src/main/java/org/techtown/seminar2/presentation/CalculatorActivity.kt
+++ app/src/main/java/org/techtown/seminar2/presentation/CalculatorActivity.kt
@@ -9,15 +9,16 @@ import org.techtown.seminar2.databinding.ActivityCarculatorBinding
 import org.techtown.seminar2.viewmodel.MyNumberViewModel
 
 class CalculatorActivity : AppCompatActivity() {
-    private val myNumberViewModel: MyNumberViewModel by viewModels()
+//    private val myNumberViewModel: MyNumberViewModel by viewModels()
     private lateinit var binding: ActivityCarculatorBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_carculator)
-        // 뷰 모델을 바인딩 변수로 사용한다.
-        binding.viewmodel = myNumberViewModel
-        // binding에 LifecycleOwner 지정 시 LiveData가 실시간으로 변하는 것을 확인 가능하다
-        binding.lifecycleOwner = this@CalculatorActivity
+        binding = ActivityCarculatorBinding.inflate(layoutInflater)
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_carculator)
+//        // 뷰 모델을 바인딩 변수로 사용한다.
+//        binding.viewmodel = myNumberViewModel
+//        // binding에 LifecycleOwner 지정 시 LiveData가 실시간으로 변하는 것을 확인 가능하다
+//        binding.lifecycleOwner = this@CalculatorActivity
     }
 }
