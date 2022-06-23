7b9aca047ca0b97a27922d77d1584aeb3b39aa18
murjune <sis9221302@gmail.com>
2022-06-24 04:50:24 +0900

    [FEAT] two-way 데이터바인딩 사용

--- app/src/main/java/org/techtown/seminar2/presentation/viewmodel/BindingAdapters.kt
+++ app/src/main/java/org/techtown/seminar2/presentation/viewmodel/BindingAdapters.kt
@@ -3,17 +3,8 @@ package org.techtown.seminar2.presentation.viewmodel
 import android.widget.ProgressBar
 import androidx.databinding.BindingAdapter
 
-// 속성값을 2개이상 넣어주고 싶을 때는 다음과 같이 쓴다.
-// 아래 3가지 속성을 동시에 관찰하면서, 변화가 있을 때
-// progress바의 증가량 or 감소량을  cnt의 2배로 하고, 임계치값 설정
 @BindingAdapter(value = ["app:progressScaled", "android:max", "android:min"], requireAll = true)
 fun setProgress(progressBar: ProgressBar, count: Int, max: Int, min: Int) {
     progressBar.progress = (count * 2).coerceIn(min, max)
 }
-// Int클래스의 Extension
-// public fun Int.coerceIn(minimumValue: Int, maximumValue: Int): Int {
-//    if (minimumValue > maximumValue) throw IllegalArgumentException("Cannot coerce value to an empty range: maximum $maximumValue is less than minimum $minimumValue.")
-//    if (this < minimumValue) return minimumValue
-//    if (this > maximumValue) return maximumValue
-//    return this
-// }
+
--- app/src/main/java/org/techtown/seminar2/presentation/viewmodel/MyNumberViewModel.kt
+++ app/src/main/java/org/techtown/seminar2/presentation/viewmodel/MyNumberViewModel.kt
@@ -11,7 +11,15 @@ class MyNumberViewModel(
     val modifiedCounter: LiveData<String> = Transformations.map(liveCounter) { counter ->
         "$counter 입니다."
     }
-//    var count = savedStateHandle.get<Int>(SAVE_STATE_KEY) ?: modifiedCounter.value
+
+    // 양방향 데이터바인딩 (지금까지는 viewmodel -> xml(뷰)로만 data를 보냈지만, 이제 xml(뷰) -> viewmodel에 data보낼 수 있다.)
+    // checkBox는 UI에서도 checked속성을 바꿔줄 수 있다.
+    // 따라서, viewmodel에 hasChecked변수를 만들어준 후
+    // two-way 데이터바인딩이 잘 일어나는지 textView로 나타내준다.
+    val _hasChecked: MutableLiveData<Boolean> = MutableLiveData(false)
+    val hasChecked: LiveData<Boolean> get() = _hasChecked
+
+    //    var count = savedStateHandle.get<Int>(SAVE_STATE_KEY) ?: modifiedCounter.value
 //
 //    private fun saveState() {
 //        savedStateHandle.set(SAVE_STATE_KEY, liveCounter.value)
